6ab1a56d78d30f627388f13fc7baff9d6edeeffe
Gustavo Braga <gubragarabelo@gmail.com>
2023-12-11 15:23:54 -0300

    feat: fixing header erro by removing <View> that was wrapping it

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,22 +1,22 @@
-
-import { Button, Text, View, XStack } from "tamagui";
+import { Button, Text, XStack } from "tamagui";
 import { StepBack, Plus } from "@tamagui/lucide-icons";
 
 const Header = ({ title = "", onAddToList = null, onBack = null }) => {
   return (
-    <View>
-      <XStack
-        flex={1}
-        flexWrap="wrap"
-        backgroundColor="#fff"
-        alignItems="center"
-        justifyContent={onAddToList ? "space-between" : "center"}
-      >
-        {onBack && <Button icon={<StepBack size="$1.5" />} onPress={onBack}></Button>}
-        <Text>{title}</Text>
-        {onAddToList && <Button icon={<Plus size="$1.5" />} onPress={onAddToList}></Button>}
-      </XStack>
-    </View>
+    <XStack
+      flexWrap="wrap"
+      backgroundColor="#fff"
+      alignItems="center"
+      justifyContent={onAddToList ? "space-between" : "center"}
+    >
+      {onBack && (
+        <Button icon={<StepBack size="$1.5" />} onPress={onBack}></Button>
+      )}
+      <Text>{title}</Text>
+      {onAddToList && (
+        <Button icon={<Plus size="$1.5" />} onPress={onAddToList}></Button>
+      )}
+    </XStack>
   );
 };
 
